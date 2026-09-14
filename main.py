73496f070e8232663700d2a826c761229e6807b3
luccacoelhos-jpg <luccacoelhos@gmail.com>
2026-09-14 12:47:31 +0000

    Etapas 1 e 2.

---
+++ main.py
@@ -0,0 +1,21 @@
+# -- 1° Etapa: Cadastro e projetos. --
+startup = {
+    "Nome": "CyberPulse Tech",
+    "Segmento": "Segurança da informação",
+    "Ano_adesao": "2026"
+}
+
+solucoes_ativas = ("Firewall IA","Scan de Vunerabiidades")
+print("STARTUP:",startup["Nome"],"Segmento:",["Segmento"])
+print("Projetos em andamento",solucoes_ativas[0])
+
+# -- 2° Etapa: Mapeamento
+# 1 = Ocupado - 0 = Livre
+bancadas = [
+       [1,0],
+       [0,1]
+]
+print("Bancada N1",[1][0])
+print("Bancada N2",[0][0])
+print("Bancada S1",[0][0])
+print("Bancada S2",[1][0])
