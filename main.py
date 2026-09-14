b4aaa764baabd0db3ad2edb6315b3cc32b3d9b56
luccacoelhos-jpg <luccacoelhos@gmail.com>
2026-09-14 15:34:56 +0000

    Etapas finais.

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ print("Bancada N1",[1][0])
 print("Bancada N2",[0][0])
 print("Bancada S1",[0][0])
 print("Bancada S2",[1][0])
+
 print("1 = Ocupado - 0 = Livre.")
 
 # -- 3° Etapa: Leitura Sequencial do CSV --
